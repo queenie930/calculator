141dedbfc399436d21ce7b108be89e1661a6e0d6
Ryry.C <125869887+queenie930@users.noreply.github.com>
2023-04-27 15:27:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function calc(){
+    var a= parseInt( document.querySelector("display").value);  
+    var a=  document.querySelector("display").value;  
+    
+   var calculator;
+
+   if(int=="AC"){
+       calculator = a;
+    } else if(int=="DEL"){
+        calculator=a;
+    }else if(int=="%"){
+        calculator=a;
+    }else if(int=="/"){
+        calculator=a;
+    }
+    if(int=="7"){
+        calculator=a;
+    }else if(int=="8"){
+        calculator=a;
+    }else if(int=="9"){
+        calculator=a;
+    }else if(int=="*"){
+        calculator=a;}
+
+        if(int=="4"){
+            calculator=a;
+        }else if(int=="5"){
+            calculator=a;
+        }else if(int=="6"){
+            calculator=a;
+        }else if(int=="-"){
+            calculator=a;
+    }
+   
+    if(int=="1"){
+        calculator=a;
+    }else if(int=="2"){
+        calculator=a;
+    }else if(int=="3"){
+        calculator=a;
+    }else if(int=="+"){
+        calculator=a;}
+
+        if(int=="0"){
+            calculator=a;
+        }else if(int=="00"){
+            calculator=a;
+        }else if(int=="."){
+            calculator=a;
+        }else if(int=="="){
+            calculator=a;}
+
+
+      document.querySelector("result").InnerHTML=calculator;
+       console.log("calculator");
+   
+        }
